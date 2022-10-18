8a6067123cd9dce54a00eeff7aa5e3afb8a45940
Triki Salma <salma.triki98@gmail.com>
2022-10-18 19:41:44 +0100

    Like

---
+++ Web-fun/Core-assignment/Week2/Like/script.js
@@ -0,0 +1,26 @@
+var x =9;
+
+var sum= document.querySelector(".Likes");
+
+function add() {
+    x++;
+    sum.innerText= x +" "+"like(s)";
+}
+
+var x1 =12;
+
+var sum1= document.querySelector(".Likes2");
+
+function add2() {
+    x1++;
+    sum1.innerText= x1 +" "+"like(s)";
+}
+
+var x2 =9;
+
+var sum2= document.querySelector(".Likes3");
+
+function add3() {
+    x2++;
+    sum2.innerText= x2+" "+"like(s)";
+}
